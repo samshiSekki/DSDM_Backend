6d11f81cb107d9a0568f5d2a3303d7eaf7bae45e
Song-EunJu <jd06280@naver.com>
2022-08-26 19:58:30 +0900

    [Fix] 동아리 조건 필터링 코드 로직 오류 수정

--- src/clubs/schemas/club.service.ts
+++ src/clubs/schemas/club.service.ts
@@ -20,10 +20,10 @@ export class ClubService {
   }
 
   async getClubs(@Query() query) {
-    const allClub = new Array();
     let allClubs = await this.clubRepository.getAllClub(); // 전체 데이터 다가져옴
     const { category, recruiting, period, activityDay, online } = query;
 
+    // 카테고리로 한번 필터링
     if(category){
       const categories = category.indexOf(",")>=0 ? category.split(",") : category;
       allClubs = allClubs.filter(club => categories.includes(club.mainCategory))
@@ -39,29 +39,17 @@ export class ClubService {
       allClubs = allClubs.filter(club => onlineStatus.includes((club.online).toString()))
     }
 
+    // 3개월 이하 / 4-6개월 / 7개월-1년 / 1년 이상 ( period = 1,2,3,4)
+    console.log("3개필터링"+allClubs.length)
 
-// 3개월
-// 1년
-// 5개월
-// 2년
-// 3학기 -> 1년 반 
-
-// 학기단위 모집, 1년 활동시 정회원 자격 부여
-// 23년 1학기까지
-
-// 17주 = 4개월 
-
-
-    // 3개월 이하 / 4-6개월 / 7개월-1년 / 1년 이상
-    // period=1,2,3,4
+    // filteredClubByPeriod -> 기간필터링된 배열 저장 
     let filteredClubByPeriod = new Array();
-
+    let copiedClubs;
     if(period){
       const periods = period.indexOf(",")>=0 ? period.split(",") : period;
       const regex = /[^0-9]/g;
-      let copiedClubs = JSON.parse(JSON.stringify(allClubs));
-      // 1,2 개월 -> 6개월 미만인거 다 ...
-      for(let i=0;i<periods.length;i++){
+      copiedClubs = JSON.parse(JSON.stringify(allClubs)); // 전체 데이터 깊은 복사 -> copiedClubs
+      for(let i=0;i<periods.length;i++){ // 필터링 기간 배열 
         if(periods[i] == 1){
           for(let club of copiedClubs){
             let period = club.period
@@ -69,11 +57,9 @@ export class ClubService {
               let result = parseInt((period || "").replace(regex, ""))
               if(result <= 3 && period.includes("개월")){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
               else if(result/4 <=3 && period.includes("주")){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               } 
             }
           }
@@ -83,18 +69,14 @@ export class ClubService {
             let period = club.period
             if(period != null){
               let result = parseInt((period || "").replace(regex, "")) // 숫자만 추출
-      
               if((result >=4 && result<=6) && period.includes("개월")){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
               else if(period.includes("학기") && result == 1){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
               else if(period.includes("주") && result/4 <=6){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
             }
           }
@@ -104,14 +86,11 @@ export class ClubService {
             let period = club.period
             if(period != null){ 
               let result = parseInt((period || "").replace(regex, "")) // 숫자만 추출
-      
               if((result >=7 && result<=11) && period.includes("개월")){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
               else if((result/4 >=7 && result/4 <=11) && period.includes("주")){
                 filteredClubByPeriod.push(club);
-                copiedClubs.splice(i,1);
               }
             }
           }
@@ -121,11 +100,12 @@ export class ClubService {
             let period = club.period
             if(period != null){ 
               let result = parseInt((period || "").replace(regex, "")) // 숫자만 추출
-      
-              if((result >= 12) && period.includes("개월"))
+              if((result >= 12) && period.includes("개월")){
                 filteredClubByPeriod.push(club);
-              else if((result/4 >=12) && period.includes("주"))
+              }
+              else if((result/4 >=12) && period.includes("주")){
                 filteredClubByPeriod.push(club);
+              }
               else if((result >= 2) && period.includes("학기")){
                 filteredClubByPeriod.push(club);
               }
@@ -136,53 +116,136 @@ export class ClubService {
           }
         }
       }
-      return filteredClubByPeriod
     }
 
-    let filteredClub = new Array();
+//    console.log(filteredClubByPeriod)
 
-    if(activityDay){
-      const activityDays = activityDay.indexOf(",")>=0 ? activityDay.split(",") : activityDay;
-    
-      if(!(activityDay.indexOf(",")>=0))
-        allClubs = allClubs.filter(club => (club.activityDay).includes(activityDay));
-      else {
-        for(let club of allClubs){
-          for(let activityDay of activityDays){
-            if((club.activityDay).includes(activityDay)){
-              filteredClub.push(club);
-              break;
+    // filteredClubByActivityDay -> 활동일자로 필터링
+    let filteredClubByActivityDay = new Array();
+
+    if(filteredClubByPeriod.length == 0) { // 기간 필터링이 안됐으면 -> allClubs 로 필터링해야함
+      console.log("기간 필터링 xx")
+      if(activityDay){
+        const activityDays = activityDay.indexOf(",")>=0 ? activityDay.split(",") : activityDay;
+        
+        console.log(...activityDays) // 여기가 이상함 
+        console.log(activityDays.length)
+        if(!(activityDay.indexOf(",")>=0)){
+          console.log("오나?")
+          allClubs = allClubs.filter(club => (club.activityDay).includes(activityDay));
+        }
+        else {
+          for(let club of allClubs){
+            for(let activityDay of activityDays){
+              if((club.activityDay).includes(activityDay)){
+                allClubs.push(club);
+                break;
+              }
             }
           }
         }
       }
     }
-
-    if(filteredClub.length == 0 )
-      return allClubs;
-    
-    for(let club of filteredClub){
-      const {mainCategory} = club;
-      allClub[mainCategory] = new Array();
-    }
-
-    console.log(allClub)
-    for(let club of filteredClub){
-      const {clubId, name, recruiting, subCategory, membershipFee, online, period, activityDay, selectionProcess} = club;
-      const clubInfo = {
-          clubId,
-          name,
-          subCategory,
-          recruiting: (recruiting == true) ?'모집중':'마감',
-          membershipFee,
-          online: (online == 1)?'온라인':((online==2)?'오프라인':'온/오프라인'),
-          period,
-          activityDay,
-          selectionProcess
+    else{ // 기간 필터링이 됐으면 -> filteredClubByPeriod 로 필터링해야함
+      console.log("기간 필터링 00")
+      if(activityDay){
+        const activityDays = activityDay.indexOf(",")>=0 ? activityDay.split(",") : activityDay;
+        
+        console.log(...activityDays) // 여기가 이상함 
+        console.log(activityDays.length)
+        if(!(activityDay.indexOf(",")>=0)){
+          console.log("오나?")
+          filteredClubByPeriod = filteredClubByPeriod.filter(club => (club.activityDay).includes(activityDay));
+          console.log("byperiod2"+filteredClubByPeriod.length);
+        }
+        else {
+          for(let club of filteredClubByPeriod){
+            for(let activityDay of activityDays){
+              if((club.activityDay).includes(activityDay)){
+                filteredClubByActivityDay.push(club);
+                break;
+              }
+            }
+          }
+        }
       }
-      allClub[club.mainCategory].push(clubInfo)
     }
-    return allClub
+    let finalClub = new Object();
+
+
+// ActivityDay.length == 0 && period !=0 --> period 로 해야함 
+// activityDay.length == 0 && period == 0 --> allclubs로
+// activityDay.length !=0 && period == 0 -> allclubs
+// acitivyday.length !=0 && peiord !=0 -> activity로 
+
+    if(filteredClubByPeriod.length==0){ // filtered 로 해야함 
+      for(let club of allClubs){
+        const {mainCategory} = club;
+        finalClub[mainCategory] = new Array();
+      }
+      
+      for(let club of allClubs){
+        const {clubId, name, recruiting, subCategory, membershipFee, online, period, activityDay, selectionProcess} = club;
+        const clubInfo = {
+            clubId,
+            name,
+            subCategory,
+            recruiting: (recruiting == true) ?'모집중':'마감',
+            membershipFee,
+            online: (online == 1)?'온라인':((online==2)?'오프라인':'온/오프라인'),
+            period,
+            activityDay,
+            selectionProcess
+        }
+        finalClub[club.mainCategory].push(clubInfo)
+      }
+    } 
+    else if(filteredClubByPeriod.length !=0 && filteredClubByActivityDay.length ==0){
+      for(let club of filteredClubByPeriod){
+        const {mainCategory} = club;
+        finalClub[mainCategory] = new Array();
+      }
+
+      for(let club of filteredClubByPeriod){
+        const {clubId, name, recruiting, subCategory, membershipFee, online, period, activityDay, selectionProcess} = club;
+        const clubInfo = {
+            clubId,
+            name,
+            subCategory,
+            recruiting: (recruiting == true) ?'모집중':'마감',
+            membershipFee,
+            online: (online == 1)?'온라인':((online==2)?'오프라인':'온/오프라인'),
+            period,
+            activityDay,
+            selectionProcess
+        }
+        finalClub[club.mainCategory].push(clubInfo)
+
+      }
+    }
+    else if(filteredClubByPeriod.length !=0 && filteredClubByActivityDay.length !=0){
+      for(let club of filteredClubByActivityDay){
+        const {mainCategory} = club;
+        finalClub[mainCategory] = new Array();
+      }
+      
+      for(let club of filteredClubByActivityDay){
+        const {clubId, name, recruiting, subCategory, membershipFee, online, period, activityDay, selectionProcess} = club;
+        const clubInfo = {
+            clubId,
+            name,
+            subCategory,
+            recruiting: (recruiting == true) ?'모집중':'마감',
+            membershipFee,
+            online: (online == 1)?'온라인':((online==2)?'오프라인':'온/오프라인'),
+            period,
+            activityDay,
+            selectionProcess
+        }
+        finalClub[club.mainCategory].push(clubInfo)
+      }
+    }
+    return finalClub
   }
 
   async getClubsToday() {
